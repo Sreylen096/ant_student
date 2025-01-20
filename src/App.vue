c1e3b5e3a390bf61f6fb7d61daff3fd7027b89ba
Sreylen096 <heangsreylen096@gmail.com>
2025-01-20 09:32:06 +0700

    Update responsive

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,6 @@
 
 <script setup>
 import { RouterView, useRouter } from 'vue-router'
-import { onMounted, onUnmounted } from 'vue';
 
-const resetOverflow = () => {
-    document.body.style.overflow = 'auto'
-}
 
-onMounted( () => resetOverflow());
-onUnmounted( () => resetOverflow());
 </script>
